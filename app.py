a1b9d50645c9ada01508bfb431d2bd07762507e1
Djegui-lab <wdjegui45@gmail.com>
2024-11-15 14:03:47 +0100

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
+import streamlit as st
 
-# Définir le chemin du fichier JSON directement
-json_file_path = "C:\projet\courtier-devis-automatique-e47e170f58f7.json"
+# Définir le chemin relatif du fichier JSON
+json_file_path = "courtier-devis-automatique-e47e170f58f7.json"
 
 # Vérifiez si le fichier existe
 if not os.path.exists(json_file_path):
-    raise FileNotFoundError(f"Le fichier JSON à l'emplacement {json_file_path} est introuvable.")
-
-print(f"Le fichier JSON a été trouvé à l'emplacement : {json_file_path}")
+    st.error(f"Le fichier JSON à l'emplacement {json_file_path} est introuvable.")
+else:
+    st.success(f"Le fichier JSON a été trouvé à l'emplacement : {json_file_path}")
